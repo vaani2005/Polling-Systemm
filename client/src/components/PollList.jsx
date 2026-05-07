ea5994f5521653718cdf5cbc3a72ef92eb3e5c55
Vaani <vaani@webguruz.in>
2026-05-07 11:38:58 +0530

    api

--- client/src/components/PollList.jsx
+++ client/src/components/PollList.jsx
@@ -30,7 +30,9 @@ export default function PollList() {
       setLoading(true);
 
       const res = await fetch(
-        `http://localhost:5000/poll?page=${pageNum}&limit=5&search=${search}&filter=${filter}`,
+        // `http://localhost:5000/poll?page=${pageNum}&limit=5&search=${search}&filter=${filter}`,
+        `https://your-backend.onrender.com/poll?page=${pageNum}&limit=5&search=${search}&filter=${filter}`,
+
         {
           headers: {
             Authorization: `Bearer ${token}`,
